4cb92d8aab918742f7013f252e3ec65be5a1ee35
qdu <q.du@connect.ust.hk>
2023-12-15 15:41:38 +0800

    Exclude "false" folder in install script

--- install.jl
+++ install.jl
@@ -2,7 +2,12 @@ using Pkg
 
 Pkg.activate()
 
+run(`pip3 install pymssql`)
+
 for name in readdir(@__DIR__)
+    if name == "false"
+        continue
+    end
     dir = joinpath(@__DIR__, name)
     if isdir(dir) && !startswith(name, ".")
         Pkg.develop(path=dir)
