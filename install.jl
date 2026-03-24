090c2166452dfa86095b13e696187d895c251cf9
qdu <qdduuu@gmail.com>
2026-03-24 17:53:41 +0800

    Improve install script robustness

--- install.jl
+++ install.jl
@@ -1,12 +1,27 @@
 using Pkg, Logging
 
-@info "Installing Python dependencies"
-ENV["PYTHON"] = Sys.which("python3")
-run(`pip3 install -U pip`)
-run(`pip3 install pymssql`)
-Pkg.activate("JlUtils")
-Pkg.update()
+const JLUTILS_GIT_URL = get(ENV, "JLUTILS_GIT_URL", "https://github.com/qdddddd/julia-utils.git")
+const JLUTILS_GIT_REV = get(ENV, "JLUTILS_GIT_REV", "master")
+const PYTHON_BIN = something(Sys.which("python3"), Sys.which("python"), nothing)
+
+if PYTHON_BIN === nothing
+    error("python3/python not found in PATH")
+end
+
+@info "Installing Python dependencies" python=PYTHON_BIN
+ENV["PYTHON"] = PYTHON_BIN
+run(`$PYTHON_BIN -m pip install -U pip`)
+run(`$PYTHON_BIN -m pip install pymssql`)
+
+@info "Installing JlUtils from git" url=JLUTILS_GIT_URL rev=JLUTILS_GIT_REV
 Pkg.activate()
-Pkg.develop(path=@__DIR__)
+try
+    Pkg.rm("JlUtils")
+catch err
+    @info "JlUtils was not already installed in the active environment" exception=(err, catch_backtrace())
+end
+Pkg.add(url=JLUTILS_GIT_URL, rev=JLUTILS_GIT_REV)
 Pkg.build()
 Pkg.precompile()
+Pkg.status(["JlUtils"])
+
